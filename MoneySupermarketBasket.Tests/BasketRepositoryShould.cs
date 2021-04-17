d4f560a9112cd8a97bcc25eecd0c9d8d1d863aed
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2021-04-17 15:23:37 +0100

    BasketRepositoryShould add two items to basket

--- MoneySupermarketBasket.Tests/BasketRepositoryShould.cs
+++ MoneySupermarketBasket.Tests/BasketRepositoryShould.cs
@@ -21,7 +21,25 @@ namespace MoneySupermarketBasket.Tests
 
             List<BasketItem> actual = basketRepository.GetBasketItems();
             Assert.Single(actual);
+        }
+
+        [Fact]
+        public void Add_two_items_to_basket()
+        {
+            var p1 = new Product("Milk", 2.50);
+            var p2 = new Product("Bread", 2.50);
+            var item = new BasketItem(p1, 3);
+            var item2 = new BasketItem(p2, 3);
+            var basketRepository = new BasketRepository();
+
+            basketRepository.AddItem(item);
+            basketRepository.AddItem(item2);
+
+            var actual = basketRepository.GetBasketItems();
+            Assert.Equal(2, actual.Count);
 
         }
+
+
     }
 }
