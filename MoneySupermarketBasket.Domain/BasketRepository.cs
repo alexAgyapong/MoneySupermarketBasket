cacb6f483042ca8e955ff0f6b816e02f671f9d8b
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2021-04-17 15:21:01 +0100

    BasketRepositoryShould add one item to basket

---
+++ MoneySupermarketBasket.Domain/BasketRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoneySupermarketBasket.Domain
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly List<BasketItem> basketItems = new();
+        public BasketRepository()
+        {
+        }
+
+        public void AddItem(BasketItem item)
+        {
+            basketItems.Add(item);
+        }
+
+        public List<BasketItem> GetBasketItems() => basketItems;
+    }
+}
---
+++ MoneySupermarketBasket.Tests/BasketRepositoryShould.cs
@@ -0,0 +1,27 @@
+﻿using MoneySupermarketBasket.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MoneySupermarketBasket.Tests
+{
+    public class BasketRepositoryShould
+    {
+        [Fact]
+        public void Add_one_item_to_basket()
+        {
+            var p1 = new Product("Milk", 2.50);
+            var item = new BasketItem(p1, 3);
+            var basketRepository = new BasketRepository();
+
+            basketRepository.AddItem(item);
+
+            List<BasketItem> actual = basketRepository.GetBasketItems();
+            Assert.Single(actual);
+
+        }
+    }
+}
