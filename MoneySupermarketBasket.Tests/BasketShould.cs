b293b1f5a39b6eae48f02b98b8d9b903becbd82b
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2021-04-17 14:46:17 +0100

    Add project files.

---
+++ MoneySupermarketBasket.Tests/BasketShould.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MoneySupermarketBasket.Tests
+{
+    public class BasketShould
+    {
+        [Fact]
+        public void Be_Truthy()
+        {
+            Assert.True(true);
+        }
+    }
+}
